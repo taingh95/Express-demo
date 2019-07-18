a195d6985d05f124cd48952963a8fdcad9a4b860
taingh95 <taingh95@gmail.com>
2019-07-18 22:13:22 +0700

    Express-demo-2

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req,res) => res.send('<h1>Hello world</h1>'))
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "Tai"
+  });
+});
+
+app.get('/users', (req,res) => {
+    res.render("users/index", {
+        users: [
+            {name: "Tai", id: "1"},
+            {name: "Linh", id: "2"}
+        ]
+    })
+})
 
 
-app.listen(port, () => console.log(`App is running on port ${3000}`))
+app.listen(port, () => console.log(`App is running on port ${3000}`));
