7fe2183fb3d637d89ca35db96ea0c11b7c45298a
taingh95 <taingh95@gmail.com>
2019-07-18 23:02:04 +0700

    query parameters

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 
 const port = 3000;
 
+
+//data
+let users =  [
+    {name: "Tai", id: "1"},
+    {name: "Linh", id: "2"}
+]
+
+
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -14,12 +23,21 @@ app.get("/", (req, res) => {
 
 app.get('/users', (req,res) => {
     res.render("users/index", {
-        users: [
-            {name: "Tai", id: "1"},
-            {name: "Linh", id: "2"}
-        ]
+        users
     })
 })
+
+app.get('/users/search', (req,res) => {
+    let q = req.query.q;
+    let matchedUser = users.filter((user) => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    })
+    res.render('users/index', {
+        users: matchedUser
+    })
+    console.log(req.query)
+})
+
 
 
 app.listen(port, () => console.log(`App is running on port ${3000}`));
